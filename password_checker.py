9f47d19c398416ed01b68f1d319b8206b5c49a99
Jackson.Dev <ryanjackson1005@gmail.com>
2026-07-04 19:02:23 -0300

    feat: add SENHAS_COMUNS and CORES constants

---
+++ password_checker.py
@@ -0,0 +1,15 @@
+
+#-------------------------
+#      Constantes
+#-------------------------
+
+SENHAS_COMUNS = [
+"1234556", "password", "123456789", "12345678", "12345", "1234567", "password1", "qwerty",
+"abc123", "111111", "iloveyou", "admin", "letmain", "monkey", "1234567890", "senha", "brasil",
+"admin123", "senhasegura", "root"
+]
+
+COR = {
+"vermelho": "\033[91m", "amarelo": "033[93m", "verde": "\033[92m", "ciano": "\033[96m",
+"branco": "\033[97", "reset": "\033[0m", "negrito": "\033[1m", 
+}
