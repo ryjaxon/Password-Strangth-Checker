3c6df74f5e87d77d24bb5f10c76ad6bdba4aac84
Jackson.Dev <ryanjackson1005@gmail.com>
2026-07-04 18:27:02 -0300

    feat: create password_checker.py with base structure

---
+++ password_checker.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+"""
+=====================================
+      Password Strangth Checker
+  Criado por: Ryjaxon (Rian Jackson)
+=====================================
+ferramenta para analise de força de senhas.
+"""
+
+improt re
+improt sys
